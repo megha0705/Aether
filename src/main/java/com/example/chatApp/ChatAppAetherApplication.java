f38e723485c94edaaec99b2e83e45b5442bcc2c0
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-05-05 22:03:35 +0530

    Initial Spring Boot commit

---
+++ src/main/java/com/example/chatApp/ChatAppAetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppAetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppAetherApplication.class, args);
+	}
+
+}
